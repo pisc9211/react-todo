52be5c93aa959935226f4ab4a7353b9fb95e6848
pisc9211 <pisc9211@gmail.com>
2019-04-10 19:47:46 -0400

    Updates

---
+++ precache-manifest.23e6933cd15884d8b410f327a5e8fe26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b8624abea7e9229d964",
+    "url": "/react-todo/static/css/main.6db12d71.chunk.css"
+  },
+  {
+    "revision": "6b8624abea7e9229d964",
+    "url": "/react-todo/static/js/main.1722be44.chunk.js"
+  },
+  {
+    "revision": "052fad23ca9f036fcf47",
+    "url": "/react-todo/static/js/runtime~main.79666361.js"
+  },
+  {
+    "revision": "af1b6b08e2a705d0b8e7",
+    "url": "/react-todo/static/js/2.7b0e938e.chunk.js"
+  },
+  {
+    "revision": "e5d6067617ea70fc9789fa18d4a216b1",
+    "url": "/react-todo/index.html"
+  }
+];
